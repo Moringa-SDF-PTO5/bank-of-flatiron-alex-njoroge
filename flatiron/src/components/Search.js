a36965c6f982b0d45a127b51fcdbe58047439c10
Alex Njoroge <alex.njoroge@student.moringaschool.com>
2024-03-19 15:00:43 -0700

    In Progress

---
+++ flatiron/src/components/Search.js
@@ -0,0 +1,22 @@
+//Filter transactions based on descriptions
+
+import React from 'react';
+
+function Search({ onSearchChange }) {
+  const handleSearchChange = (event) => {
+    // Call the function passed as prop with the new search value
+    onSearchChange(event.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search by description..."
+        onChange={handleSearchChange}
+      />
+    </div>
+  );
+}
+
+export default Search;
